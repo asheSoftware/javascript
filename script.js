42c936d651d3a0e42d0676901ab7683b87b44b72
asheSoftware <ashenafigc2021@gmail.com>
2022-06-17 08:29:07 +0300

    javascript variable

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-document.writeln("hello from external js");
-alert("hello form external js");
+/*document.writeln("hello from external js");
+alert("hello form external js");*/
 //consile log statement
 console.log("hello form console");
+let name="Ashenafi Tesfaye";
+//javascript variable contain digit,alphabet,underscore and dollar sign 
+//start with letter,_ and $ not start in digit
+//const ,let,var is the keyword
+let $address,state,zip;
+$address="Addis Abeba";
+state="Ethiopian";
+zip="200002"
+console.log(name);
+console.log($address);
+console.log(state);
+console.log(zip);
